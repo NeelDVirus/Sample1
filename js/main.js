8181b0755c96aa505be934be58d8b46badaf5d99
NeelDVirus <neelunstp@gmail.com>
2017-11-09 17:08:40 +0530

    Hamburger-icon: modified: animation

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
 ------------------------------------------------*/
     var $hamburger = $(".nav-button");    
     $hamburger.on("click", function(e){
-        $hamburger.toggleClass("is-active");
+        $hamburger.toggleClass("is-active change-height"); //change height is added by me
     });
 
 
 /*------------------------------------------------
 --- TRIGGERING__ HAMBURGER-ICON__MOBILE-MENU -----
 ------------------------------------------------*/
-    var $menuLeft = $("#cbp-spmenu-s1");
-    var $showLeft = $("#showLeft");
+    var $menuLeft = $("#cbp-spmenu-s2");
+    var $showLeft = $("#showRight");
     $showLeft.on("click", function(e){
         $menuLeft.toggleClass("cbp-spmenu-open");
     });
