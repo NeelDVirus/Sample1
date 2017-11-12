3c1548dda710254594a98d9f7e1f536e8a27470f
NeelDVirus <neelunstp@gmail.com>
2017-11-12 15:47:26 +0530

    Event-Scroll:  bug-present

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 
+
 /*------------------------------------------------
-----------   MOBILE-MENU__ HAMBURGER-ICON    ----------
+--- PAGE-SCROLLING EFFECT ON HEADER COMPONENT -----
+------------------------------------------------*/
+
+    var $headerComponent = $(".header-wrapper");
+    $(window).on("scroll", function(){
+        var displacementFromTop = window.pageYOffset || document.documentElement.scrollTop;
+        if (displacementFromTop > 0) {
+            $headerComponent.removeClass("unscrolled");
+        } else {
+            $headerComponent.addClass("unscrolled");
+        } 
+    });
+
+/*------------------------------------------------
+----------   HAMBURGER-ICON    ----------
 ------------------------------------------------*/
     var $hamburger = $(".nav-button");    
     $hamburger.on("click", function(e){
@@ -10,12 +25,14 @@ $(document).ready(function(){
 
 
 /*------------------------------------------------
---- TRIGGERING__ HAMBURGER-ICON__MOBILE-MENU -----
+--- TRIGGERING__ MOBILE-MENU_ VIA_ HAMBURGER-ICON__
 ------------------------------------------------*/
     var $menuLeft = $("#cbp-spmenu-s2");
     var $showLeft = $("#showRight");
     $showLeft.on("click", function(e){
         $menuLeft.toggleClass("cbp-spmenu-open");
     });
+
+
 
 });
